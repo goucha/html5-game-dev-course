61ad47119e6b283df5d54c45cc8b19b058c07e6f
Michal Pasierbski <michal.pasierbski@sooftly.com>
2013-02-19 22:33:15 +0100

    setup

---
+++ public/javascripts/application.js
@@ -0,0 +1,10 @@
+(function () {
+  var canvas = document.getElementById("game-canvas");
+
+  function init() {
+    canvas.setAttribute("width", "700");
+    canvas.setAttribute("height", "500");
+  }
+
+  init();
+})();
